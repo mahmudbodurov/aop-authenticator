32c82f92f2c4a2fce69c53309a23ac80efaefff3
Mahmud Bodurov <bodurovmahmud@gmail.com>
2023-05-09 00:05:27 +0400

    Readme changes

--- src/main/java/com/mb/tool/aop/authenticator/annotations/AuthenticationUser.java
+++ src/main/java/com/mb/tool/aop/authenticator/annotations/AuthenticationUser.java
@@ -1,6 +1,10 @@
 package com.mb.tool.aop.authenticator.annotations;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Inherited
 @Target({ElementType.PARAMETER})
