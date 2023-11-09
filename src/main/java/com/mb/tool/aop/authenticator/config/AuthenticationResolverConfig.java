9ed95d1b0a8f15f086293212fc884ceefda804dd
Mahmud Bodurov <bodurovmahmud@gmail.com>
2023-11-10 00:01:15 +0400

    HandlerMethodArgumentResolver configuration added

--- src/main/java/com/mb/tool/aop/authenticator/config/AuthenticationResolverConfig.java
+++ src/main/java/com/mb/tool/aop/authenticator/config/AuthenticationResolverConfig.java
@@ -5,10 +5,12 @@ import com.mb.tool.aop.authenticator.AuthenticationHandler;
 import com.mb.tool.aop.authenticator.AuthenticationResolver;
 import com.mb.tool.aop.authenticator.AuthenticationUserMethodHandler;
 import com.mb.tool.aop.authenticator.NoOpAuthenticationResolver;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 @ConditionalOnClass(AuthenticationHandler.class)
@@ -27,9 +29,10 @@ public class AuthenticationResolverConfig {
     }
 
     @Bean
-    @ConditionalOnMissingBean(AuthenticationHandler.class)
-    public AuthenticationUserMethodHandler authMethodHandler(AuthenticationResolver authenticationResolver) {
-        return new AuthenticationUserMethodHandler(authenticationResolver);
+    @ConditionalOnMissingBean(CustomRequestParamMethodArgumentResolver.class)
+    @ConditionalOnBean(AuthenticationResolver.class)
+    public CustomRequestParamMethodArgumentResolver customRequestParamMethodArgumentResolver(@Lazy AuthenticationResolver authenticationResolver) {
+        return new CustomRequestParamMethodArgumentResolver(new AuthenticationUserMethodHandler(authenticationResolver));
     }
 
 }
